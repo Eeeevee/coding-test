e98f21ee8e64ca9c463f03b2b8607e06bf93b57e
Eevee <156054454+Eeeevee@users.noreply.github.com>
2024-03-13 19:48:08 +0900

    [level 1] Title: 문자열 나누기, Time: 0.63 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,18 +1,18 @@
 class Solution {
     public int solution(String s) {
         int answer = 1;
-        char x = s.charAt(0);
-        int count = 1;
+        char c = s.charAt(0);
+        int cnt = 1;
         for (int i = 1; i < s.length(); i++) {
-            if (count == 0) {
+            if (cnt == 0) {
                 answer++;
-                x = s.charAt(i);
+                c = s.charAt(i);
             }
 
-            if (x == s.charAt(i)) {
-                count++;
+            if (c == s.charAt(i)) {
+                cnt++;
             } else {
-                count--;
+                cnt--;
             }
         }
         return answer;
