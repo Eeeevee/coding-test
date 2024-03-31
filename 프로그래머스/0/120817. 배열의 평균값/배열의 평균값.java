1e5ac0fa3745bbe54edc0a590b75c4837805a3ab
Eevee <156054454+Eeeevee@users.noreply.github.com>
2024-03-31 23:13:56 +0900

    [level 0] Title: 배열의 평균값, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -4,9 +4,10 @@ class Solution {
         int answer = 0;
 
         for (int i = 0; i < numbers.length; i++) {
+            
             answer += numbers[i];
         }
-
+        
         return (double) answer / numbers.length;
     }
 }
