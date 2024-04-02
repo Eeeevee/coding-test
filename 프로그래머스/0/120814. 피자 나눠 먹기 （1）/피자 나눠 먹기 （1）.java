60ee17449b7df8d4899771a8f8b3a6124200d6de
Eevee <156054454+Eeeevee@users.noreply.github.com>
2024-04-03 04:27:20 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.04 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        double answer = (double) n / 7;
+        
+        if ((double) n % 7 > 0) {
+            answer += 1;
+        }
+        
+        return (int) answer;
+    }
+}
