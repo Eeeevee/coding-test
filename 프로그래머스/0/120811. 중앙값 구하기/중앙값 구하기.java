eb2db28493f2634a53ae7671f62832a6c0483833
Eevee <156054454+Eeeevee@users.noreply.github.com>
2024-04-26 21:04:43 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.50 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -2,7 +2,9 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] array) {
+        
         Arrays.sort(array);
+        
         return array[array.length / 2];
     }
 }
