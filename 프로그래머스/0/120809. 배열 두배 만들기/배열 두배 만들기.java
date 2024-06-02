3aae81a96ff5b8850eab22b3c08467ed3b432507
Eevee <156054454+Eeeevee@users.noreply.github.com>
2024-06-02 20:51:21 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.05 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] numbers) {
+        
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] *= 2;
         }
